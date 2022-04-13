c6c40d837d818438390b72b1220a605829fd0cb3
Narain Yucel <yucelnarain@gmail.com>
2022-04-13 18:49:47 -0400

    adjustment

--- scripts/metric_2.py
+++ scripts/metric_2.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     import streamlit as st
     import pandas as pd
     import numpy as np
-        import matplotlib.pyplot as plt
+    import matplotlib.pyplot as plt
     import seaborn as sns
 
 
